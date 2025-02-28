7b7c1980ded3c776a4e373c92fb79fb09f23ae9f
Aliya M <167082547+AliyaM003@users.noreply.github.com>
2025-02-28 22:07:02 +0530

    Create sublist2.py

---
+++ sublist2.py
@@ -0,0 +1,7 @@
+colors={"darkyellow","orange","yellow","red"}
+color_lengths={color:len(color)
+                for color in colors}
+print(color_lengths)
+
+#OUTPUT
+{'darkyellow': 10, 'red': 3, 'yellow': 6, 'orange': 6}
