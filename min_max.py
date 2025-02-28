2f5ca960e8491e05ccbc4103e193a831832aff43
Aliya M <167082547+AliyaM003@users.noreply.github.com>
2025-02-28 22:00:00 +0530

    Create min_max.py

---
+++ min_max.py
@@ -0,0 +1,6 @@
+def find_max_min(numbers):
+    return min(numbers),max(numbers)
+numbers=[2,3,9,23,57]
+min_value,max_value=find_max_min(numbers)
+print("min number",min_value)
+print("max number",max_value)
