b8ced1498468467341186e2f6528f9c90c099787
Aliya M <167082547+AliyaM003@users.noreply.github.com>
2025-02-28 22:13:53 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,10 @@
+n=8
+if n>1:
+    for i in range(2,int(n/2)+1):
+        if i%1==0:
+            print("not a prime number")
+        else:
+            print("prime number")
+
+#OUTPUT
+8 is not a prime number
